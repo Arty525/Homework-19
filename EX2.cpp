5400cc00e8ae955963af12fe266bf6b842066078
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-07 16:33:23 +0300

    Add files via upload

---
+++ EX2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string convert(string address) {
+	string buffer;
+	for (int i = 0; i < address.length(); ++i) {
+		buffer += address[i];
+		if (address[i] == '\\' && address[i - 1] != '\\' && address[i + 1] != '\\') {
+			buffer += '\\';
+		}
+	}
+	return buffer;
+}
+
+int main() {
+	string address = "D:\\RTY\\GITHUB\\Repositories\\homework-19\\EX1\\textfile.txt";
+	//string text;
+	char chunk[1];
+
+	//cout << "Input file address: ";
+	//cin >> address;
+
+	ifstream file;
+	file.open(convert(address), ios::binary);
+	if (file.is_open()) {
+		cout << "File is open." << endl;
+	}
+	else {
+		cout << "File is not open." << endl;
+		return main();
+	}
+	while (!file.eof()) {
+		file.read(chunk, sizeof(chunk));
+		cout << chunk;
+	}
+	file.close();
+	return 0;
+}
