4f36b4eb51d4d0ecf140b8fa753a318116100ac2
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-09 15:31:16 +0300

    Update EX2.cpp

--- EX2/EX2.cpp
+++ EX2/EX2.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+string convert(string address) {
+	string buffer;
+	for (int i = 0; i < address.length(); ++i) {
+		buffer += address[i];
+		if (address[i] == '\\' && address[i - 1] != '\\' && address[i + 1] != '\\') {
+			buffer += '\\';
+		}
+	}
+	return buffer;
+}
+
+int main() {
+	string address;
+	string text;
+	char chunk[64];
+
+	cout << "Input file address: ";
+	cin >> address;
+
+	ifstream file;
+	file.open(convert(address), ios::binary);
+	if (file.is_open()) {
+		cout << "File is open." << endl;
+	}
+	else {
+		cout << "File is not open." << endl;
+		return main();
+	}
+	while (getline(file, text))
+	{
+		cout << text << endl;
+	}
+	file.close();
+	return 0;
+}
