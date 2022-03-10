e591dcfd87a1b141be0040bab8a15c9dbc37885c
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-10 14:57:00 +0300

    Add files via upload

--- EX3/EX3.cpp
+++ EX3/EX3.cpp
@@ -1,1 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+int main() {
+	string maxName;
+	string firstName;
+	string secondName;
+	int pay;
+	string date;
+	int max = 0;
+	int summ = 0;
+
+	ifstream statement;
+	statement.open("D:\\RTY\\GITHUB\\Repositories\\homework-19\\EX3\\statement.txt");
+	while (!statement.eof()) {
+
+		statement >> firstName >> secondName >> pay >> date;
+		cout << firstName << " " << secondName << " " << pay << " " << date << endl << endl;
+		summ += pay;
+		if (pay > max) {
+			max = pay;
+			maxName = firstName + " " + secondName + " " + to_string(max) + " " + date;
+		}
+	}
+	statement.close();
+
+	cout << "Max pay: " << maxName << endl;
+	cout << "Summary pay: " << summ << endl;
+	return 0;
+
+}
